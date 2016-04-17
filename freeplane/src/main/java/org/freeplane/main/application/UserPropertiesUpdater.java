167c150f1be0729b7eacb0381db2bce23ce0bc85
Dimitry Polivaev <dimitry.polivaev@mercateo.com>
2016-04-17 09:54:16 +0200

    Squashed alpha-1.4.1-pre06
    commit 7bd9a9597ea3ea0ac45528f6c51cc778bd6c4e73
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 21:57:15 2015 +0100
    
        Do not select previously selected node after cancel editing because it
        happens in undo now
    
    commit 6a8e51f7796a77f6da4dafa29ffb9f17bf154ba3
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 21:43:31 2015 +0100
    
        Allow new summary nodes if firstGroupNode was set in the middle
    
    commit 0abdb3ea0aadd2a2f34ffcdd0d5ce63b8a678b99
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 21:29:15 2015 +0100
    
        Do not stop editing twice and force transaction
    
    commit e646e9b0ed76a68f11deb8810757e8cb5fad1a7d
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 20:04:18 2015 +0100
    
        Fix exe file names produced by build
    
    commit bbcc0c0b1c8aa273bf89d72537499566b51e0d16
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 19:16:54 2015 +0100
    
        consider minor version property and create git tag from gradle release task
    
    commit 6c261401dab5c6c8fc472a1c99a7b5deed4a3bf4
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 19:08:00 2015 +0100
    
        fix MenuBuilderAcceptanceTest
    
    commit dd6731fd93004b81f77a6c58fc2b641ac64ef2f4
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 18:34:07 2015 +0100
    
        Keep HTML in shortened text
    
    commit 7989f6d483d0b3d1c1f8cd9a74b75b99ed15530e
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 17:54:21 2015 +0100
    
        Focus node content before node is minimized or maximized
    
    commit 3333853130222cd6eea71951b46dc02d56837438
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 17:50:55 2015 +0100
    
        remove unused method
    
    commit 86e91c356fb8778b286558962187f0e394c323d2
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 17:48:25 2015 +0100
    
        repaint node view after update
    
    commit dc61fc3974b0a31f9bfe73983d28a406b1ea4f56
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 17:35:27 2015 +0100
    
        Scroll to optimal visible rectangle on start and on back to root
    
    commit 4bb432097aa2447faf17a6feed9f3029defbae8b
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 16:56:22 2015 +0100
    
        make predefinedStyleParentNode  a bubble
    
    commit 7613f05a934fd81de662d3f9175cd8fc613bc719
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 16:25:15 2015 +0100
    
        Do not validate and scroll map view on set size if size is not changed
    
    commit 81d8697a1eea62a1f96dc640507c19c6a0131942
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 16:16:11 2015 +0100
    
        Different properties for foldingsymbolwidth and statesymbolwidth
    
    commit 4011e057c422f18f2a077caedac868fadec423d1
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 15:59:25 2015 +0100
    
        Revert unintended change in MainView
    
    commit 342d2767c3785a41f284f4c915aa97d88b15f2c5
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 14:10:14 2015 +0100
    
        Fix execute menu item links
    
    commit bbe3ea9376fff281a5a3e9e03d04ef3c7acfb277
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 13:42:07 2015 +0100
    
        Remove specific menu items on MAC OS
    
    commit 81f9062ced4582dcf6e804aae39750bc8314aa5b
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 13:32:39 2015 +0100
    
        Fix item selection in MapPopup and NodePopup
    
    commit fc0741a345cf08ef371445653c30361c21a772cb
    Author: Dimitry Polivaev <dpolivaev@gmx.de>
    Date:   Fri Dec 4 13:09:00 2015 +0100
    
        Remove MenuBuilder
    
        // FIXME Compat.macMenuChanges()
        // FIXME LinkController.loadURL(NodeModel, ActionEvent, URI)
        // FIXME AddOnDetailsPanel.formatMenuLocation(Script)

---
+++ freeplane/src/main/java/org/freeplane/main/application/UserPropertiesUpdater.java
@@ -0,0 +1,233 @@
+/*
+ *  Freeplane - mind map editor
+ *  Copyright (C) 2010 dimitry
+ *
+ *  This file author is dimitry
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.freeplane.main.application;
+
+import java.awt.Font;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.swing.JLabel;
+
+import org.freeplane.core.resources.ResourceController;
+import org.freeplane.core.util.Compat;
+import org.freeplane.core.util.LogUtils;
+import org.freeplane.features.edge.EdgeModel;
+import org.freeplane.features.map.MapModel;
+import org.freeplane.features.map.NodeModel;
+import org.freeplane.features.mode.Controller;
+import org.freeplane.features.mode.ModeController;
+import org.freeplane.features.mode.mindmapmode.MModeController;
+import org.freeplane.features.nodestyle.NodeSizeModel;
+import org.freeplane.features.nodestyle.NodeStyleController;
+import org.freeplane.features.nodestyle.NodeStyleModel;
+import org.freeplane.features.note.mindmapmode.MNoteController;
+import org.freeplane.features.styles.MapStyleModel;
+import org.freeplane.features.url.mindmapmode.MFileManager;
+
+/**
+ * @author Dimitry Polivaev
+ * Nov 1, 2010
+ */
+public class UserPropertiesUpdater {
+	private static final String ORG_FREEPLANE_OLD_USERFPDIR = "org.freeplane.old_userfpdir";
+
+	void importOldProperties(){
+		final File userPreferencesFile = ApplicationResourceController.getUserPreferencesFile();
+		if(userPreferencesFile.exists()){
+			return;
+		}
+		copyUserFilesFromPreviousVersionTo(userPreferencesFile.getParentFile());
+		if(userPreferencesFile.exists()){
+			try {
+				Properties userProp = loadProperties(userPreferencesFile);
+				removeVersionSpecificProperties(userProp);
+				saveProperties(userProp, userPreferencesFile);
+			}
+			catch (IOException e) {
+			}
+		}
+	}
+
+	private void copyUserFilesFromPreviousVersionTo(File targetDirectory) {
+		final File parentDirectory = targetDirectory.getParentFile();
+		final File sourceDirectory;
+		String old_userfpdir = System.getProperty(ORG_FREEPLANE_OLD_USERFPDIR);
+		if (isDefined(old_userfpdir))
+			sourceDirectory = new File(old_userfpdir, Compat.PREVIOUS_VERSION_DIR_NAME);
+		else
+			sourceDirectory = new File(parentDirectory, Compat.PREVIOUS_VERSION_DIR_NAME);
+		if (sourceDirectory.exists() && !sourceDirectory.getAbsolutePath().equals(targetDirectory.getAbsolutePath())) {
+			try {
+				parentDirectory.mkdirs();
+				org.apache.commons.io.FileUtils.copyDirectory(sourceDirectory, targetDirectory);
+			}
+			catch (IOException e) {
+			}
+			return;
+		}
+    }
+
+	private boolean isDefined(String old_userfpdir) {
+	    return old_userfpdir != null;
+    }
+
+	private void removeVersionSpecificProperties(Properties userProp) {
+		for(String name : new String[]{
+				"lastOpened_1.0.20",
+				"openedNow_1.0.20",
+				"openedNow_1.3.04",
+				"browse_url_storage",
+				"single_backup_directory_path",
+		"standard_template"})
+			userProp.remove(name);
+	}
+
+
+	Properties loadProperties(File userPreferencesFile) throws IOException {
+	    FileInputStream inputStream = null;
+	    Properties userProp = new Properties();
+	    try{
+	    inputStream = new FileInputStream(userPreferencesFile);
+	    userProp.load(inputStream);
+	    }
+	    finally {
+	    	org.freeplane.core.util.FileUtils.silentlyClose(inputStream);
+	    }
+	    return userProp;
+    }
+
+	void saveProperties(Properties userProp, File userPreferencesFile) throws IOException {
+	    FileOutputStream outputStream = null;
+	    try{
+	    	outputStream = new FileOutputStream(userPreferencesFile);
+	    	userProp.store(outputStream, null);
+	    }
+	    finally {
+	    	org.freeplane.core.util.FileUtils.silentlyClose(outputStream);
+	    }
+    }
+
+	void createUserStandardTemplate() {
+		final ModeController modeController = Controller.getCurrentController().getModeController(MModeController.MODENAME);
+		MFileManager fm = MFileManager.getController(modeController);
+		final ResourceController resourceController = ResourceController.getResourceController();
+		final String standardTemplateName = resourceController.getProperty(MFileManager.STANDARD_TEMPLATE);
+		File userDefault;
+		final File absolute = new File(standardTemplateName);
+		final File userTemplates = fm.defaultUserTemplateDir();
+		if(absolute.isAbsolute())
+			userDefault = absolute;
+		else{
+			userDefault= new File(userTemplates, standardTemplateName);
+		}
+		if(userDefault.exists()){
+			return;
+		}
+		userDefault.getParentFile().mkdirs();
+		if(! userDefault.getParentFile().exists()){
+			return;
+		}
+		MapModel defaultStyleMap = new MapModel();
+		final File allUserTemplates = fm.defaultStandardTemplateDir();
+		File standardTemplate = new File(allUserTemplates, standardTemplateName);
+		if(! standardTemplate.exists()) {
+			final String defaultStandardTemplate = resourceController.getDefaultProperty(MFileManager.STANDARD_TEMPLATE);
+			resourceController.setProperty(MFileManager.STANDARD_TEMPLATE, defaultStandardTemplate);
+			standardTemplate = new File(allUserTemplates, defaultStandardTemplate);
+			userDefault = new File(userTemplates, standardTemplateName);
+			if(userDefault.exists()){
+				return;
+			}
+		}
+		try {
+			fm.loadCatchExceptions(standardTemplate.toURL(), defaultStyleMap);
+		}
+		catch (Exception e) {
+			LogUtils.warn(e);
+			try {
+				fm.loadCatchExceptions(resourceController.getResource("/styles/viewer_standard.mm"), defaultStyleMap);
+			}
+			catch (Exception e2) {
+				defaultStyleMap.createNewRoot();
+				LogUtils.severe(e);
+			}
+		}
+        final NodeStyleController nodeStyleController = NodeStyleController.getController(modeController);
+        updateDefaultStyle(nodeStyleController, defaultStyleMap);
+        updateNoteStyle(nodeStyleController, defaultStyleMap);
+
+        try {
+	        fm.writeToFile(defaultStyleMap, userDefault);
+        }
+        catch (IOException e) {
+        }
+
+
+	}
+   private void updateDefaultStyle(final NodeStyleController nodeStyleController, MapModel defaultStyleMap) {
+        NodeModel styleNode1 = MapStyleModel.getExtension(defaultStyleMap).getStyleNode(MapStyleModel.DEFAULT_STYLE);
+		NodeModel styleNode = styleNode1;
+		styleNode.removeExtension(NodeStyleModel.class);
+		styleNode.removeExtension(EdgeModel.class);
+
+		final NodeStyleModel nodeStyleModel = new NodeStyleModel();
+
+		nodeStyleModel.setBackgroundColor(nodeStyleController.getBackgroundColor(styleNode));
+		nodeStyleModel.setBold(nodeStyleController.isBold(styleNode));
+		nodeStyleModel.setColor(nodeStyleController.getColor(styleNode));
+		nodeStyleModel.setFontFamilyName(nodeStyleController.getFontFamilyName(styleNode));
+		nodeStyleModel.setFontSize(nodeStyleController.getFontSize(styleNode));
+		nodeStyleModel.setItalic(nodeStyleController.isItalic(styleNode));
+		nodeStyleModel.setShape(nodeStyleController.getShape(styleNode));
+
+		styleNode.addExtension(nodeStyleModel);
+
+		final NodeSizeModel nodeSizeModel = new NodeSizeModel();
+		nodeSizeModel.setMaxNodeWidth(nodeStyleController.getMaxWidth(styleNode));
+		nodeSizeModel.setMinNodeWidth(nodeStyleController.getMinWidth(styleNode));
+
+		final EdgeModel standardEdgeModel = EdgeModel.getModel(styleNode);
+		if(standardEdgeModel != null){
+			final EdgeModel edgeModel = new EdgeModel();
+			edgeModel.setColor(standardEdgeModel.getColor());
+			edgeModel.setStyle(standardEdgeModel.getStyle());
+			edgeModel.setWidth(standardEdgeModel.getWidth());
+			styleNode.addExtension(edgeModel);
+		}
+    }
+
+   private void updateNoteStyle(final NodeStyleController nodeStyleController, MapModel defaultStyleMap) {
+       if (ResourceController.getResourceController().getBooleanProperty((MNoteController.RESOURCES_USE_DEFAULT_FONT_FOR_NOTES_TOO)))
+           return;
+       final NodeModel styleNode = MapStyleModel.getExtension(defaultStyleMap).getStyleNode(MapStyleModel.NOTE_STYLE);
+       if(styleNode == null)
+           return;
+       styleNode.removeExtension(NodeStyleModel.class);
+       final Font defaultFont = new JLabel().getFont();
+       final NodeStyleModel nodeStyleModel = new NodeStyleModel();
+       nodeStyleModel.setFontFamilyName(defaultFont.getFamily());
+       nodeStyleModel.setFontSize(defaultFont.getSize());
+       styleNode.addExtension(nodeStyleModel);
+   }
+}
+
