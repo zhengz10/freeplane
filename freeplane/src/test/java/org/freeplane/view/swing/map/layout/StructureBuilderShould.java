68a1dffdc203d35cc12a4ff9bc5fb0216ee1a49c
Dimitry Polivaev <dpolivaev@gmx.de>
2017-01-14 11:16:42 +0100

    Make StructureBuilder test independent from element type

--- freeplane/src/test/java/org/freeplane/view/swing/map/layout/StructureBuilderShould.java
+++ freeplane/src/test/java/org/freeplane/view/swing/map/layout/StructureBuilderShould.java
@@ -8,15 +8,15 @@ import org.junit.Test;
 
 public class StructureBuilderShould {
 	private int nodeCount = 0;
-	private SingleLayoutElement element() {
+	private SingleElement element() {
 		return element("e" + ++nodeCount);
 	}
 	
-	private SingleLayoutElement element(final String name) {
+	private SingleElement element(final String name) {
 		return new Node(name);
 	}
 	
-	private static class Node extends SingleLayoutElement {
+	private static class Node extends SingleElement {
 		private final String s;
 
 		private Node(String s) {
@@ -30,134 +30,134 @@ public class StructureBuilderShould {
 	}
 
 
-	private StructureBuilder<LayoutElement> uut;
+	private StructureBuilder<Element> uut;
 
 	@Before
 	public void setup() throws Exception {
-		uut = new StructureBuilder<LayoutElement>(new EmptyLayoutElement(), new PairFactory<LayoutElement>() {
+		uut = new StructureBuilder<Element>(new EmptyElement(), new PairFactory<Element>() {
 
 			@Override
-			public LayoutElement createPair(LayoutElement first, LayoutElement second) {
-				return new LayoutElementPair(first, second);
+			public Element createPair(Element first, Element second) {
+				return new ElementPair(first, second);
 			}
 		}, 
-				new PairFactory<LayoutElement>() {
+				new PairFactory<Element>() {
 
 			@Override
-			public LayoutElement createPair(LayoutElement first, LayoutElement second) {
-				return new LayoutSummaryPair(first, second);
+			public Element createPair(Element first, Element second) {
+				return new SummaryPair(first, second);
 			}
 		});
 	}
 
 	@Test
 	public void createEmptyLayoutElement() throws Exception {
-		LayoutElement builtElement = uut.build();
-		assertThat(builtElement).isExactlyInstanceOf(EmptyLayoutElement.class);
+		Element builtElement = uut.build();
+		assertThat(builtElement).isExactlyInstanceOf(EmptyElement.class);
 	}
 
 	@Test
 	public void createElementWithSingleNode() throws Exception {
-		final SingleLayoutElement singleLayoutElement = element();
-		LayoutElement builtElement = uut.with(singleLayoutElement).build();
+		final SingleElement singleLayoutElement = element();
+		Element builtElement = uut.with(singleLayoutElement).build();
 		assertThat(builtElement).isSameAs(singleLayoutElement);
 	}
 
 
 	@Test
 	public void createElementWithTwoSingleNodes() throws Exception {
-		final SingleLayoutElement firstLayoutElement = element();
-		final SingleLayoutElement secondLayoutElement = element();
-		LayoutElement builtElement = uut.with(firstLayoutElement).with(secondLayoutElement).build();
-		assertThat(builtElement).isExactlyInstanceOf(LayoutElementPair.class);
-		assertThat(new LayoutElementPair(firstLayoutElement, secondLayoutElement))
+		final SingleElement firstLayoutElement = element();
+		final SingleElement secondLayoutElement = element();
+		Element builtElement = uut.with(firstLayoutElement).with(secondLayoutElement).build();
+		assertThat(builtElement).isExactlyInstanceOf(ElementPair.class);
+		assertThat(new ElementPair(firstLayoutElement, secondLayoutElement))
 			.isEqualToComparingFieldByField(builtElement);
 	}
 
 
 	@Test
 	public void createElementWithSummary() throws Exception {
-		final SingleLayoutElement firstLayoutElement = element();
-		final SingleLayoutElement summaryLayoutElement = element();
-		LayoutElement builtElement = uut.beginGroup().with(firstLayoutElement).withSummary(summaryLayoutElement).build();
-		assertThat(builtElement).isExactlyInstanceOf(LayoutSummaryPair.class);
-		assertThat(new LayoutSummaryPair(firstLayoutElement, summaryLayoutElement))
+		final SingleElement firstLayoutElement = element();
+		final SingleElement summaryLayoutElement = element();
+		Element builtElement = uut.beginGroup().with(firstLayoutElement).withSummary(summaryLayoutElement).build();
+		assertThat(builtElement).isExactlyInstanceOf(SummaryPair.class);
+		assertThat(new SummaryPair(firstLayoutElement, summaryLayoutElement))
 			.isEqualToComparingFieldByField(builtElement);
 	}
 
 	@Test
 	public void createElementWithSummaryFollowedBySingleElement() throws Exception {
-		final SingleLayoutElement firstLayoutElement = element();
-		final SingleLayoutElement summaryLayoutElement = element();
-		final SingleLayoutElement otherLayoutElement = element();
-		LayoutElement builtElement = uut.beginGroup().with(firstLayoutElement).withSummary(summaryLayoutElement).with(otherLayoutElement).build();
-		assertThat(builtElement).isExactlyInstanceOf(LayoutElementPair.class);
-		assertThat(new LayoutElementPair(new LayoutSummaryPair(firstLayoutElement, summaryLayoutElement), otherLayoutElement))
+		final SingleElement firstLayoutElement = element();
+		final SingleElement summaryLayoutElement = element();
+		final SingleElement otherLayoutElement = element();
+		Element builtElement = uut.beginGroup().with(firstLayoutElement).withSummary(summaryLayoutElement).with(otherLayoutElement).build();
+		assertThat(builtElement).isExactlyInstanceOf(ElementPair.class);
+		assertThat(new ElementPair(new SummaryPair(firstLayoutElement, summaryLayoutElement), otherLayoutElement))
 			.isEqualToComparingFieldByFieldRecursively(builtElement);
 	}
 
 	@Test
 	public void createElementFollowedByElementWithSummary() throws Exception {
-		final SingleLayoutElement firstLayoutElement = element();
-		final SingleLayoutElement secondLayoutElement = element();
-		final SingleLayoutElement summaryLayoutElement = element();
-		LayoutElement builtElement = uut.with(firstLayoutElement).beginGroup().with(secondLayoutElement).withSummary(summaryLayoutElement).build();
-		assertThat(new LayoutElementPair(firstLayoutElement, new LayoutSummaryPair(secondLayoutElement, summaryLayoutElement)))
+		final SingleElement firstLayoutElement = element();
+		final SingleElement secondLayoutElement = element();
+		final SingleElement summaryLayoutElement = element();
+		Element builtElement = uut.with(firstLayoutElement).beginGroup().with(secondLayoutElement).withSummary(summaryLayoutElement).build();
+		assertThat(new ElementPair(firstLayoutElement, new SummaryPair(secondLayoutElement, summaryLayoutElement)))
 			.isEqualToComparingFieldByFieldRecursively(builtElement);
 	}
 
 	@Test
 	public void createElementWithSummaryOfSummary() throws Exception {
-		final SingleLayoutElement firstLayoutElement = element();
-		final SingleLayoutElement summaryLayoutElement = element();
-		final SingleLayoutElement secondSummaryLayoutElement = element();
-		LayoutElement builtElement = uut.beginGroup().with(firstLayoutElement).withSummaryBeginGroup(summaryLayoutElement).withSummary(secondSummaryLayoutElement).build();
-		assertThat(new LayoutSummaryPair(new LayoutSummaryPair(firstLayoutElement, summaryLayoutElement), secondSummaryLayoutElement))
+		final SingleElement firstLayoutElement = element();
+		final SingleElement summaryLayoutElement = element();
+		final SingleElement secondSummaryLayoutElement = element();
+		Element builtElement = uut.beginGroup().with(firstLayoutElement).withSummaryBeginGroup(summaryLayoutElement).withSummary(secondSummaryLayoutElement).build();
+		assertThat(new SummaryPair(new SummaryPair(firstLayoutElement, summaryLayoutElement), secondSummaryLayoutElement))
 			.isEqualToComparingFieldByFieldRecursively(builtElement);
 	}
 
 	@Test
 	public void createElementWithSummaryOfSummaryOfSummary() throws Exception {
-		final SingleLayoutElement firstLayoutElement = element();
-		final SingleLayoutElement summaryLayoutElement = element();
-		final SingleLayoutElement secondSummaryLayoutElement = element();
-		final SingleLayoutElement thirdSummaryLayoutElement = element();
-		LayoutElement builtElement = uut.beginGroup().with(firstLayoutElement).withSummaryBeginGroup(summaryLayoutElement).withSummaryBeginGroup(secondSummaryLayoutElement).withSummary(thirdSummaryLayoutElement).build();
-		assertThat(new LayoutSummaryPair(new LayoutSummaryPair(new LayoutSummaryPair(firstLayoutElement, summaryLayoutElement), secondSummaryLayoutElement), thirdSummaryLayoutElement))
+		final SingleElement firstLayoutElement = element();
+		final SingleElement summaryLayoutElement = element();
+		final SingleElement secondSummaryLayoutElement = element();
+		final SingleElement thirdSummaryLayoutElement = element();
+		Element builtElement = uut.beginGroup().with(firstLayoutElement).withSummaryBeginGroup(summaryLayoutElement).withSummaryBeginGroup(secondSummaryLayoutElement).withSummary(thirdSummaryLayoutElement).build();
+		assertThat(new SummaryPair(new SummaryPair(new SummaryPair(firstLayoutElement, summaryLayoutElement), secondSummaryLayoutElement), thirdSummaryLayoutElement))
 			.isEqualToComparingFieldByFieldRecursively(builtElement);
 	}
 
 	@Test
 	public void createElementWithSummaryOfTwoSummaries() throws Exception {
-		final SingleLayoutElement firstLayoutElement = element();
-		final SingleLayoutElement firstSummaryLayoutElement = element();
-		final SingleLayoutElement secondLayoutElement = element();
-		final SingleLayoutElement secondSummaryLayoutElement = element();
-		final SingleLayoutElement secondLevelSummaryLayoutElement = element();
-		LayoutElement builtElement = uut.beginGroup().with(firstLayoutElement).withSummaryBeginGroup(firstSummaryLayoutElement)
+		final SingleElement firstLayoutElement = element();
+		final SingleElement firstSummaryLayoutElement = element();
+		final SingleElement secondLayoutElement = element();
+		final SingleElement secondSummaryLayoutElement = element();
+		final SingleElement secondLevelSummaryLayoutElement = element();
+		Element builtElement = uut.beginGroup().with(firstLayoutElement).withSummaryBeginGroup(firstSummaryLayoutElement)
 				.beginGroup().with(secondLayoutElement).withSummary(secondSummaryLayoutElement).withSummary(secondLevelSummaryLayoutElement).build();
-		assertThat(new LayoutSummaryPair(
-				new LayoutElementPair (
-						new LayoutSummaryPair(firstLayoutElement, firstSummaryLayoutElement),
-						new LayoutSummaryPair(secondLayoutElement, secondSummaryLayoutElement)), secondLevelSummaryLayoutElement))
+		assertThat(new SummaryPair(
+				new ElementPair (
+						new SummaryPair(firstLayoutElement, firstSummaryLayoutElement),
+						new SummaryPair(secondLayoutElement, secondSummaryLayoutElement)), secondLevelSummaryLayoutElement))
 			.isEqualToComparingFieldByFieldRecursively(builtElement);
 	}
 	
 
 	@Test
 	public void finalTest() throws Exception {
-		final SingleLayoutElement e1 = element();
-		final SingleLayoutElement e2 = element();
-		final SingleLayoutElement e3 = element();
-		final SingleLayoutElement e4 = element();
-		final SingleLayoutElement e5 = element();
-		final SingleLayoutElement e6 = element();
-		final SingleLayoutElement e7 = element();
-		final SingleLayoutElement e8 = element();
-		final SingleLayoutElement e9 = element();
-		final SingleLayoutElement e10 = element();
+		final SingleElement e1 = element();
+		final SingleElement e2 = element();
+		final SingleElement e3 = element();
+		final SingleElement e4 = element();
+		final SingleElement e5 = element();
+		final SingleElement e6 = element();
+		final SingleElement e7 = element();
+		final SingleElement e8 = element();
+		final SingleElement e9 = element();
+		final SingleElement e10 = element();
 		
-		LayoutElement builtElement = uut.with(e1).beginGroup().with(e2).with(e3).withSummaryBeginGroup(e4)
+		Element builtElement = uut.with(e1).beginGroup().with(e2).with(e3).withSummaryBeginGroup(e4)
 				.with(e5)
 				.beginGroup().with(e6).with(e7).withSummary(e8).withSummary(e9).with(e10).build();
 		
@@ -167,4 +167,60 @@ public class StructureBuilderShould {
 		
 	}
 	
+}
+
+interface Element{
+	
+}
+
+class SummaryPair implements Element {
+
+	private Element first;
+	private Element second;
+
+	public SummaryPair(Element firstLayoutElement, Element secondLayoutElement) {
+		this.first = firstLayoutElement;
+		this.second = secondLayoutElement;
+	}
+
+	@Override
+	public String toString() {
+		return "{" + first + ":" + second + "}";
+	}
+}
+
+class ElementPair implements Element {
+
+	private Element first;
+	private Element second;
+
+	public ElementPair(Element firstLayoutElement, Element layoutSummaryPair) {
+		this.first = firstLayoutElement;
+		this.second = layoutSummaryPair;
+	}
+
+	@Override
+	public String toString() {
+		return first + "," + second;
+	}
+
+}
+
+
+class SingleElement implements Element {
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+
+}
+
+class EmptyElement implements Element {
+
 }
